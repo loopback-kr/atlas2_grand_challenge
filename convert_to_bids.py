e9309ef0aadeab332f72b7791225f0b73ee100af
Hyunseok Lim <hyunseoki@outlook.kr>
2023-02-20 14:21:37 +0900

    Refactoring converting BIDS

--- convert_to_bids.py
+++ convert_to_bids.py
@@ -1,15 +1,15 @@
-import os, numpy as np, nibabel as nib
-from os.path import join, basename, dirname, exists
-from tqdm import tqdm
+# Python Built-in libraries
+import os, sys, json, csv, re, random, numpy as np
+from os.path import join, basename, exists, splitext, dirname, isdir
+from shutil import copy, copytree, rmtree
 from glob import glob, iglob
+from tqdm import tqdm, trange
+from tqdm.contrib import tzip
+# Medical imaging libraries
+import nibabel as nib, SimpleITK as sitk
+# BIDS
 from bidsio import BIDSLoader
 from settings import eval_settings
-from tqdm.contrib import tzip
-
-# Define global vars
-model_prediction_dir = os.getenv('MODEL_PREDICTION_DIR', default=None)
-BIDS_formatted_dir = 'BIDS_' + model_prediction_dir.split('/')[-1]
-zip_filename = f'{BIDS_formatted_dir}.zip'
 
 
 def check_integrity(src_dir: str, fname='*.nii.gz', chk_label_1=False):
@@ -32,17 +32,17 @@ def chk_CRC(src_dir:str, fname:str='*', recursive:bool=True):
         except Exception as e:
             tqdm.write(path+': '+e.__str__())
 
-def convert_to_BIDS(src_dir:str, dst_dir:str, fname:str='*.nii.gz'):
-    for path in tqdm(sorted(glob(join(src_dir, fname))), desc='Convert to BIDS', colour='green', dynamic_ncols=True):
-        # Directory configuration
-        tqdm.write(path)
-        first_dir = 'sub-r'+basename(path)[3:6]+'s'+basename(path)[7:10]
-        target_dir = join(dst_dir, first_dir, 'ses-1', 'anat')
-        os.makedirs(target_dir, exist_ok=True)
-        fname = join(f'{first_dir}_{"ses-1"}_{"space-MNI152NLin2009aSym_label_L_desc-T1lesion_mask.nii.gz"}')
+def convert_to_bids_format(src_dir:str, dst_root_dir:str, fname:str='*.nii.gz'):
+    for path in tqdm(sorted(glob(join(src_dir, fname))), desc=f'Convert to BIDS format', colour='green', dynamic_ncols=True):
+        fname = basename(path).split('.nii.gz')[0]
+        dst_fname = fname + '_ses-1_space-MNI152NLin2009aSym_label_L_desc-T1lesion_mask.nii.gz'
+        dst_dir = join(dst_root_dir, fname, 'ses-1', 'anat')
+        os.makedirs(dst_dir, exist_ok=True)
+        fname = join(f'{fname}_{"ses-1_space-MNI152NLin2009aSym_label_L_desc-T1lesion_mask.nii.gz"}')
 
+        tqdm.write(f'{src_dir}/{basename(path)} >> {dst_dir}/{dst_fname}')
         # Save as NiBabel file        
-        nib.load(path).to_filename(join(target_dir, fname))
+        nib.load(path).to_filename(join(dst_dir, dst_fname))
 
 def union(src0_dir:str, src1_dir:str, dst_dir:str):
     os.makedirs(dst_dir, exist_ok=True)
@@ -66,12 +66,21 @@ def union(src0_dir:str, src1_dir:str, dst_dir:str):
 
 
 if __name__ == '__main__':
-    if exists(zip_filename):
-        os.remove(zip_filename)
-    # union(union_src0_dir, union_src1_dir, union_dst_dir)
-    convert_to_BIDS(model_prediction_dir, BIDS_formatted_dir)
-    BIDSLoader.write_dataset_description(BIDS_formatted_dir, eval_settings['PredictionBIDSDerivativeName'][0])
-    if os.getenv('CHECK_CRC', default=None) == 'True':
-        check_integrity(BIDS_formatted_dir)
-    os.system(f'cd "{BIDS_formatted_dir}" && zip -qr "../{zip_filename}" *')
-    os.system(f'rm -rf "{BIDS_formatted_dir}"')
+    if not len(sys.argv) == 2:
+        raise NotImplementedError('Pass the model_prediction_dir parameter explicitly.')
+    else:
+        src_dir = sys.argv[1]
+        if isdir(src_dir):
+            dst_dir = f'tmp/{src_dir}_BIDS'
+            dst_zip_abspath = f'{os.environ["PWD"]}/out/{src_dir}.zip'
+            os.makedirs(dirname(dst_zip_abspath), exist_ok=True)
+
+            # union(union_src0_dir, union_src1_dir, union_dst_dir)
+            convert_to_bids_format(src_dir, dst_dir)
+            BIDSLoader.write_dataset_description(dst_dir, eval_settings['PredictionBIDSDerivativeName'][0])
+            if os.getenv('CHECK_CRC', default=None) == 'True':
+                check_integrity(dst_dir)
+            
+            os.system(f'cd "{dst_dir}" && zip -qr "{dst_zip_abspath}" *')
+        else:
+            raise Exception('Specify model output directory.')
